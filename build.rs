a8a6b242fd7ad74bfeaf211fbb21d33e9512b4b7
johnbywater <john.bywater@appropriatesoftware.net>
2025-10-21 18:59:30 +0100

    Rename gRPC proto file reference from `event_store.proto` to `umadb.proto`.

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/event_store.proto")?;
+    tonic_build::compile_protos("proto/umadb.proto")?;
     Ok(())
 }
